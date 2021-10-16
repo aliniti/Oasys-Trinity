f006b08ed59b9f779992962f5f15c527786a85a5
robin <xrobinsong@gmail.com>
2021-10-15 22:57:07 -0700

    Klepto

--- Trinity/Bootstrap.cs
+++ Trinity/Bootstrap.cs
@@ -30,6 +30,7 @@
             AllItems.AddRange(ConsumableItems);
             AllItems.AddRange(CleanseItems);
             AllItems.AddRange(OffensiveItems);
+            AllItems.AddRange(KleptoItems);
 
             Initialize();
             NewHeroCache();
@@ -44,21 +45,78 @@
             CoreEvents.OnCoreMainInputAsync -= CoreEvents_OnCoreMainInputAsync;
         }
 
+        private static readonly List<ActiveItem> KleptoItems = new()
+        {
+            // item: Elixir_Of_Skill
+            new ActiveItem(100, ItemID.Elixir_Of_Skill, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue,new[] { Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Sly_Sack_of_Gold
+            new ActiveItem(100, ItemID.Sly_Sack_of_Gold, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue,new[] { Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Oracles_Extract
+            new ActiveItem(100, ItemID.Oracles_Extract, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue, new[] { Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Looted_Oracles_Extract
+            new ActiveItem(100, ItemID.Looted_Oracles_Extract, Enums.TargetingType.ProximityAlly,
+                float.MaxValue, new[] { Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Pilfered_Potion_of_Rouge
+            new ActiveItem(100, ItemID.Pilfered_Potion_of_Rouge, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue, new[] { Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Travel_Size_Elixir_of_Iron
+            new ActiveItem(100, ItemID.Travel_Size_Elixir_of_Iron, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue, new[] { Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Travel_Size_Elixir_of_Sorcery
+            new ActiveItem(100, ItemID.Travel_Size_Elixir_of_Sorcery, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue, new[] { Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Travel_Size_Elixir_of_Wrath
+            new ActiveItem(100, ItemID.Travel_Size_Elixir_of_Wrath, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue, new[] { Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Mana_Potion
+            new ActiveItem(100, ItemID.Mana_Potion, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue, new[] { Enums.ActivationType.CheckAllyLowMP, Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Pilfered_Health_Potion
+            new ActiveItem(100, ItemID.Pilfered_Health_Potion, Enums.TargetingType.ProximityAlly, 
+                float.MaxValue, new[] { Enums.ActivationType.CheckAllyLowHP, Enums.ActivationType.CheckOnlyOnMe })
+        };
+
         private static readonly List<ActiveItem> OffensiveItems = new()
         {
+            // item: Twin_Shadows
+            new ActiveItem(90, ItemID.Twin_Shadows, Enums.TargetingType.ProximityEnemy, 1100,
+                new[]  { Enums.ActivationType.CheckEnemyLowHP, Enums.ActivationType.CheckAllyLowHP }),
+
             // item: Youmuus_Ghostblade
             new ActiveItem(90, ItemID.Youmuus_Ghostblade, Enums.TargetingType.ProximityEnemy, 1100, 
                 new[] {Enums.ActivationType.CheckEnemyLowHP}),
 
             // item: Bilgewater_Cutlass
             new ActiveItem(90, ItemID.Bilgewater_Cutlass, Enums.TargetingType.UnitEnemy, 575,
-                new[]  { Enums.ActivationType.CheckEnemyLowHP, Enums.ActivationType.CheckAllyLowHP, 
-                        Enums.ActivationType.CheckOnlyOnMe }),
+                new[]  { Enums.ActivationType.CheckEnemyLowHP, Enums.ActivationType.CheckOnlyOnMe }),
 
             // item: Blade_of_the_Ruined_King
             new ActiveItem(90, ItemID.Blade_of_the_Ruined_King, Enums.TargetingType.UnitEnemy, 575,
-                new[] { Enums.ActivationType.CheckEnemyLowHP, Enums.ActivationType.CheckAllyLowHP, 
-                        Enums.ActivationType.CheckOnlyOnMe }),
+                new[] { Enums.ActivationType.CheckEnemyLowHP, Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Hextech_GLP_800
+            new ActiveItem(90, ItemID.Hextech_GLP_800, Enums.TargetingType.SkillshotEnemy, 575,
+                new[] { Enums.ActivationType.CheckEnemyLowHP, Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Hextech_Protobelt_RocketBelt
+            new ActiveItem(90, ItemID.Hextech_Protobelt_RocketBelt, Enums.TargetingType.SkillshotEnemy, 575,
+                new[] { Enums.ActivationType.CheckEnemyLowHP, Enums.ActivationType.CheckOnlyOnMe }),
+
+            // item: Hextech_Gunblade
+            new ActiveItem(90, ItemID.Hextech_Gunblade, Enums.TargetingType.UnitEnemy, 575,
+                new[] { Enums.ActivationType.CheckEnemyLowHP, Enums.ActivationType.CheckOnlyOnMe })
         };
 
         private static readonly List<ActiveItem> CleanseItems = new()
@@ -136,7 +194,6 @@
             }
 
             MenuManager.AddTab(offensseItemMenu);
-
             #endregion
 
             #region Tidy : Cosumable Item Menu
@@ -151,7 +208,6 @@
             }
 
             MenuManager.AddTab(consumablesItemMenu);
-
             #endregion
 
             #region Tidy : Cleanse Item Menu
@@ -166,7 +222,20 @@
             }
 
             MenuManager.AddTab(cleanseItemMenu);
+            #endregion
 
+            #region Tidy : Klepto Items
+
+            var kleptoItemMenu = new Tab("Trinity: Klepto");
+
+            foreach (var item in KleptoItems)
+            {
+                item.OnItemInitialize += () => InitializedTickItems.Add(item);
+                item.OnItemDispose += () => InitializedTickItems.Remove(item);
+                item.Initialize(kleptoItemMenu);
+            }
+
+            MenuManager.AddTab(kleptoItemMenu);
             #endregion
         }
 
