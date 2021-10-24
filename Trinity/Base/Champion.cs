a61e632ec0431ed1f5fd54a05cdf05e7b9eab091
Kurisu <xrobinsong@gmail.com>
2021-10-24 15:48:20 -0700

    Update Champion.cs

--- Trinity/Base/Champion.cs
+++ Trinity/Base/Champion.cs
@@ -1,17 +1,35 @@
-﻿namespace Trinity.Helpers
+﻿namespace Trinity.Base
 {
+    using System.Threading.Tasks;
     using System.Collections.Generic;
     using Oasys.Common.GameObject.Clients;
+    using Oasys.SDK.Events;
 
     public class Champion
     {
         public AIHeroClient Instance { get; set; }
         public Dictionary<string, int> AuraInfo;
+        public Dictionary<uint, Spell> ActiveSpells = new();
 
-        public Champion(AIHeroClient hero)
+        public Champion(AIHeroClient instance)
         {
-            Instance = hero;
+            Instance = instance;
             AuraInfo = new Dictionary<string, int>();
+
+            CoreEvents.OnCoreMainTick += CoreEvents_OnCoreMainTick;
+        }
+
+        private async Task CoreEvents_OnCoreMainTick()
+        {
+            if (Instance != null && Instance.IsAlive && Instance.IsCastingSpell)
+            {
+                ActiveSpells[Instance.NetworkID] = new Spell();
+            }
+        }
+
+        internal void IsGettingHitBySpell()
+        {
+
         }
     }
 }
