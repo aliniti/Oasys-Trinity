4a0068e471bbd6512be12fdaa259ca4a6a016306
robin <xrobinsong@gmail.com>
2022-05-26 13:22:10 -0700

    1.3.3 edit

---
+++ Trinity/Items/BindingItem.cs
@@ -0,0 +1,74 @@
+﻿namespace Trinity.Items
+{
+    #region
+
+    using System.Linq;
+    using Helpers;
+    using Oasys.Common.Enums.GameEnums;
+    using Oasys.Common.Extensions;
+    using Oasys.SDK.Tools;
+
+    #endregion
+
+    internal class BindingItem : ActiveItem
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BindingItem" /> class.
+        /// </summary>
+        /// <param name="usePct">The use PCT.</param>
+        /// <param name="itemId">The item identifier.</param>
+        /// <param name="tType">Type of the t.</param>
+        /// <param name="range">The range.</param>
+        /// <param name="aTypes">a types.</param>
+        /// <param name="itemBuffName">Name of the item buff.</param>
+        public BindingItem(int usePct, ItemID itemId, string itemBuffName, TargetingType tType, float range,
+            ActivationType[] aTypes) : base(usePct, itemId, tType, range, aTypes, itemBuffName)
+        {
+        }
+
+        #endregion
+
+        #region Override Methods
+
+        /// <summary>
+        ///     Called when [on tick].
+        /// </summary>
+        public override void OnTick()
+        {
+            if (!ItemSwitch[ItemId.ToString()].IsOn) return;
+            if (!SpellClass.IsSpellReady) return;
+
+            var max_hp = from t in Bootstrap.Allies
+                where t.Value.Instance.IsValidTarget(Range, false)
+                orderby t.Value.Instance.HealthPercent descending
+                select t.Value;
+
+            var most_ad = from t in Bootstrap.Allies
+                where t.Value.Instance.IsValidTarget(Range, false)
+                orderby t.Value.Instance.UnitStats.BaseAttackDamage + t.Value.Instance.UnitStats.TotalAttackDamage descending
+                select t.Value;
+
+            var units = ItemModeDisplay[ItemId + "mode"].SelectedModeName == "MostAD"
+                ? most_ad
+                : max_hp;
+
+            foreach (var ally in units)
+            {
+                if (!ally.Instance.IsMe/* && ally.Instance.IsValidTarget(Range, false)*/)
+                {
+                    if (TargetingType == TargetingType.BindingUnit && UsePct == 100)
+                        if (!ally.Instance.BuffManager.HasBuff(ItemBuffName))
+                            this.CheckItemAllyLowHealth(ally.Instance);
+
+                    if (TargetingType == TargetingType.ProximityAlly)
+                        if (ally.Instance.BuffManager.HasBuff(ItemBuffName) && ally.InWayDanger)
+                            this.CheckItemAllyLowHealth(ally.Instance);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Trinity/Spells/BindingSpell.cs
@@ -0,0 +1,83 @@
+﻿namespace Trinity.Spells
+{
+    #region
+
+    using System.Linq;
+    using Oasys.Common.Extensions;
+    using Oasys.SDK.SpellCasting;
+    using Trinity.Helpers;
+
+    #endregion
+
+    internal class BindingSpell : AutoSpell
+    {
+        #region Properties and Encapsulation
+
+        public string BoundBuffName { get; set; }
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BindingUnit" /> class.
+        /// </summary>
+        /// <param name="usePct">The use PCT.</param>
+        /// <param name="championName">Name of the champion.</param>
+        /// <param name="castSlot">The cast slot.</param>
+        /// <param name="targetingType">Type of the targeting.</param>
+        /// <param name="range">The range.</param>
+        /// <param name="activationTypes">The activation types.</param>
+        public BindingSpell(int usePct, string championName, string boundBuffName, CastSlot castSlot, TargetingType targetingType, float range,
+            ActivationType[] activationTypes)
+            : base(usePct, championName, castSlot, targetingType, range, activationTypes)
+        {
+            BoundBuffName = boundBuffName;
+        }
+
+        #endregion
+
+        #region Override Methods
+
+        /// <summary>
+        ///     Called when [OnTick].
+        /// </summary>
+        public override void OnTick()
+        {
+            var tabName = IsSummonerSpell
+                ? ChampionName
+                : ChampionName + Slot;
+
+            if (!SpellSwitch[tabName].IsOn) return;
+            if (!SpellClass.IsSpellReady) return;
+
+            var max_hp = from t in Bootstrap.Allies
+                where t.Value.Instance.IsValidTarget(Range, false)
+                orderby t.Value.Instance.HealthPercent descending
+                         select t.Value;
+
+            var most_ad = from t in Bootstrap.Allies
+                where t.Value.Instance.IsValidTarget(Range, false)
+                orderby t.Value.Instance.UnitStats.BaseAttackDamage + t.Value.Instance.UnitStats.TotalAttackDamage descending
+                          select t.Value;
+
+            var units = SpellModeDisplay[tabName + "mode"].SelectedModeName == "MostAD"
+                ? most_ad
+                : max_hp;
+
+            foreach (var ally in units)
+                if (!ally.Instance.IsMe && ally.Instance.IsValidTarget(Range, false))
+                {
+                    if (TargetingType == TargetingType.BindingUnit && UsePct == 100)
+                        if (!ally.Instance.BuffManager.HasBuff(BoundBuffName))
+                            this.CheckSpellAllyLowHealth(ally.Instance);
+
+                    if (TargetingType == TargetingType.ProximityAlly)
+                        if (ally.Instance.BuffManager.HasBuff(BoundBuffName) && ally.InWayDanger)
+                            this.CheckSpellAllyLowHealth(ally.Instance);
+                }
+        }
+
+        #endregion
+    }
+}
